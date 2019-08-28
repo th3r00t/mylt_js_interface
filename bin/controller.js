5c0e5cd8b266f036c28a575d46eb2bf8753d3d72
Admin <admin@mylocaltech.co>
2019-08-28 14:28:30 -0400

    Syncing sources

--- bin/controller.js
+++ bin/controller.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
+    first_run = true
     startConnect()
+    // Click handlers
     $("div#benchlight").click(function() {
         console.log('toggle')
         message = new Paho.MQTT.Message("0");
@@ -8,4 +10,10 @@ $(document).ready(function() {
         message.retained = false
         onConnect()
     });
+    if (first_run) {
+        if (client.isConnected()) {
+            message = new Paho.MQTT.Message("2")
+        }
+    }
+    first_run = false
 });
