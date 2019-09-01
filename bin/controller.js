99814d6aa52ee3ad511a8d49af6d4187c8d1165c
Admin <admin@mylocaltech.co>
2019-09-01 00:24:18 -0400

    Almost have working indicators

--- bin/controller.js
+++ bin/controller.js
@@ -1,7 +1,24 @@
+/* eslint-disable no-undef */
+
+window.addEventListener("focus", handleBrowserState.bind(window, true));
+window.addEventListener("blur", handleBrowserState.bind(window, false));
+
+function handleBrowserState(isActive){
+    // do something
+    if (isActive) {
+        if (client.isConnected()) {
+            return
+        } else {
+            startConnect()
+        }
+    } else {
+        client.disconnect()
+    }
+}
+
 $(document).ready(function() {
     first_run = true
     startConnect()
-        // Click handlers
     $("div#benchlight").click(function() {
         console.log('toggle')
         message = new Paho.MQTT.Message("0");
--- bin/main.js
+++ bin/main.js
@@ -1,15 +1,14 @@
+/* eslint-disable no-undef */
+// eslint-disable-next-line no-unused-vars
 function startConnect() {
-    // Generate a random client ID
     clientID = "webcontrol_" + parseInt(Math.random() * 100);
-    // Fetch the hostname/IP address and port number from the form
+    user = "guest"
+    password = "guest"
     host = "10.0.0.2"
     port = "15675";
-    // Initialize new Paho client connection
-    client = new Paho.MQTT.Client(host, Number(port), '/ws', clientID);
-    // Set callback handlers
+    client = new Paho.MQTT.Client(host, Number(port), '/ws', clientID, user, password);
     client.onConnectionLost = onConnectionLost;
     client.onMessageArrived = onMessageArrived;
-    // Connect the client, if successful, call onConnect function
     client.connect({
         onSuccess: onConnect,
         reconnect: true,
@@ -17,7 +16,6 @@ function startConnect() {
         onFailure: connectionFailed,
     });
 }
-// Called when the client connects
 function onConnect() {
     cnx_status = client.isConnected()
     console.log("Connected")
@@ -25,12 +23,13 @@ function onConnect() {
         if (message !== undefined) {
             client.send(message)
             message = undefined
+            return true
         } else {
             console.log("No Message Waiting")
         }
+        client.subscribe('lighting/')
     }
 }
-// Called when the client loses its connection
 function onConnectionLost(responseObject) {
     console.log(responseObject)
     cnx_status = client.isConnected()
@@ -40,13 +39,22 @@ function connectionFailed(invocationContext) {
     console.log("Connection Failed")
     console.log(invocationContext)
 }
-// Called when a message arrives
 function onMessageArrived(message) {
     console.log("Message: " + message.payloadString);
 }
 // Called when the disconnection button is pressed
+// eslint-disable-next-line no-unused-vars
 function startDisconnect() {
     client.disconnect();
     document.getElementById("messages").innerHTML +=
         '<span>Disconnected</span><br/>';
+}
+
+function getLightStates() {
+    message = new Paho.MQTT.Message("2")
+    message.destinationName = 'lighting/benchlight'
+    message.qos = 0
+    message.retained = false
+    onConnect()
+    client.send(message)
 }
